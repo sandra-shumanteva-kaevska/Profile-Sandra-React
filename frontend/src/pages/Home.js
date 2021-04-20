e6f0de3e9ce845db0d6d1f3679bb9a2274ddedef
Sandra Shumanteva Kaevska <sandra@shumantevi.mk>
2021-04-20 11:55:07 +0200

    home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,25 @@
-import { Box } from '@material-ui/core'
+import { Box, FormHelperText } from '@material-ui/core'
 import React from 'react'
 import myImg from '../assets/my-img.jpg'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
+    container: {
+
+
+    },
     title: {
-        color: 'white'
+        color: 'white',
+        margin: '10px',
+        float: 'left',
+        backgroundColor: '#000000a6'
+    },
+    myImg: {
+        width: '15%',
+        border: '3px solid white',
+        borderRadius: '50%',
+        float: 'right',
+        margin: '20px',
     }
 
 }))
@@ -13,9 +27,14 @@ export const Home = () => {
     const classes = useStyles()
     return (
         <Box>
-            <h1 className={classes.title}>PORTFOLIO: Sandra Shumanteva Kaevska</h1>
-            <h2 className={classes.title}>frontend developer</h2>
-            <h3 className={classes.title}>+ bank appriser</h3>
+            <Box className={classes.title}>
+                <h1 >PORTFOLIO: Sandra Shumanteva Kaevska</h1>
+                <h2 >frontend developer</h2>
+                <h3 >+ bank appriser</h3>
+            </Box>
+            <Box>
+                <img className={classes.myImg} src={myImg}></img>
+            </Box>
         </Box>
     )
 }
